ab36ee73a7a095a63c38dff4822166587f808739
Deca <deca@Soares.local>
2022-09-14 16:34:56 +0100

    chaellenge assignment-3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,27 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+ function binaryReversal(value) {
+
+   //convert string passed into  num
+  let inputValue=parseInt(value);
+  inputValue=inputValue.toString(2);
+ //console.log(inputValue)
+
+  let paddingBinary=inputValue.padStart(8,0);
+  //console.log(paddingBinary)
+  //TO reverse the binnary code
+  let reversedBinary=paddingBinary.split("").reverse().join("");
+  //console.log(reversedBinary);
+  
+  //convert it back to base 10
+  let reversedTen=parseInt(reversedBinary, 2);
+  //console.log(reversedTen)
+ return reversedBinary=reversedTen.toString();
+ //console.log(typeof reversedBinary)
+   
+
+}
+console.log(binaryReversal("200"))
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,48 @@
-function listSorting(needle, haystack) {}
+let a=[
+  [100, 200, 300],
+        [300, 200, 100],
+        [300, 100, 200],
+        [200, 100, 300],
+        [100, 200, 300],
+];
+let b = 0;
 
+function listSorting(needle, haystack) {
+let postIndex=[]
+
+for (let i = 0; i < haystack.length; i++) {
+  for (let j = 0; j < haystack[i].length; j++) {
+  //console.log(haystack[i].length);
+  if(needle == haystack[i][j]){
+      //postIndex=[j]
+     postIndex=[i,j];
+      //console.log(postIndex);
+  }
+} 
+
+
+}
+if (postIndex.length==0){
+  return -1; }
+
+return postIndex;
+}
+console.log(listSorting(b,a));
 module.exports = listSorting;
+
+
+//   let rowIndex = haystack.length - 1;
+  // let colIndex = -1;
+
+  // for (let i = haystack.length - 1; i >= 0; i--) {
+  //   if (Array.isArray(haystack[i])) {
+  //     rowIndex = i;
+  //     colIndex = haystack[i].lastIndexOf(needle);
+  //     if (colIndex != -1) {
+  //       return [rowIndex, colIndex];
+  //     }
+  //   } else {
+  //     if (haystack[i] == needle) return i;
+  //   }
+  // }
+  // return -1;
